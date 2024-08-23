e697021629b47f03a8b11a4f8ce81cf60cf48e06
Akshada <suraj.chaudhari@theleanapps.com>
2024-08-23 15:36:37 +0530

    first php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$names=array("Akshada","sharad");
+
+echo count($names);
+
+echo "<h1>My first php pratcise project</h1>";
+
+echo "Printing names array <br/>";
+foreach($names as $name){
+    echo "$name <br/>";
+}
+
+
+?>
